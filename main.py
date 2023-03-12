d5f0f9a436be79e5b4d59023e8d2affd04e7712f
mtphil <matthew.t.phillips@protonmail.com>
2023-03-12 16:56:54 -0400

    first commit

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ import os
 def main():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("-p", "--prompt", help="your prompt")
+    arg_parser.add_argument("-t", "--max_tokens", help="max tokens")
+    arg_parser.add_argument("-t", "--temperature", help="temperature")
+    arg_parser.add_argument("-f", "--frequency_penalty", help="frequency penalty")
+    arg_parser.add_argument("-m", "--model", help="model")
 
     args = arg_parser.parse_args()
 
     co = cohere.Client(os.getenv('COHERE_API_TOKEN'))
 
     response = co.generate(
-        model='xlarge',
+        model=args.model,
         prompt=args.prompt,
-        max_tokens=1000,
-        temperature=1.5,
-        frequency_penalty=0.25)
+        max_tokens=args.max_tokens,
+        temperature=args.temperature,
+        frequency_penalty=args.freqeuncy_penalty)
 
     startup_idea = response.generations[0].text
 
